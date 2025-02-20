f7eef207edce3332a1951b4cde6fe13aff73225f
henriquemilitao <henriquemilitao35@gmail.com>
2025-02-20 12:59:36 -0400

    feat: add shadcn initial setup and components

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+    return (
+    <div className="p-5 border border-red-500 rounded-xl">
+    <h1>Hello World</h1>
+    <Button>FSW 7.0</Button> 
+    </div>
+);
+}
+ 
+export default HomePage
+
+// npx shadcn@2.3.0 add button
+// npx shadcn@2.3.0 add input
+// git add .
+// git commit -m "feat: add shadcn initial setup and components"
+
+// :root {
+//     --background: 0 0% 100%;
+//     --foreground: 0 0% 20%;
+//     --card: 0 0% 100%;
+//     --card-foreground: 0 0% 20%;
+//     --popover: 0 0% 100%;
+//     --popover-foreground: 0 0% 20%;
+//     --primary: 42 100% 50%;
+//     --primary-foreground: 0 0% 98%;
+//     --secondary: 0 0% 96.1%;
+//     --secondary-foreground: 0 0% 20%;
+//     --muted: 0 0% 96.1%;
+//     --muted-foreground: 0 0% 45.1%;
+//     --accent: 0 0% 96.1%;
+//     --accent-foreground: 0 0% 20%;
+//     --destructive: 0 84.2% 60.2%;
+//     --destructive-foreground: 0 0% 98%;
+//     --border: 198 33% 94%;
+//     --input: 198 33% 94%;
+//     --ring: 0 0% 20%;
+//     --chart-1: 12 76% 61%;
+//     --chart-2: 173 58% 39%;
+//     --chart-3: 197 37% 24%;
+//     --chart-4: 43 74% 66%;
+//     --chart-5: 27 87% 67%;
+//     --radius: 0.5rem;
+//   }
+// git add .
+// git commit -m "chore: update css variables"
