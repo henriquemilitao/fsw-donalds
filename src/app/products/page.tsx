95541bd354fff1449b281fe18f05e112c1f2c8c2
henriquemilitao <henriquemilitao35@gmail.com>
2025-02-22 22:49:44 -0400

    feat: Replacing bd and add initial restaurant page and menu page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const HomePage = () => {
+  return (
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1>Hello World</h1>
+      <Button>FSW 7.0</Button>
+    </div>
+  )
+}
+
+export default HomePage
